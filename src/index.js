9e9ef6dcf1c63d31b8580cbb82d93c5fef89a161
Bjwills7 <bjwills7@hotmail.com>
2023-05-22 13:59:28 -0500

    Finish menu module Setup menu layout Add function that deletes the contents of content box before initializing new content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import { initHeader } from "./header";
 import { initHome } from "./home";
+import { initMenu } from "./menu";
 import "./style.css";
 
 initHeader();
 initHome();
+
+const homeButton = document.querySelector(".homeBtn");
+const menuButton = document.querySelector(".menuBtn");
+
+homeButton.addEventListener("click", () => {
+  clearContainer();
+  initHome();
+});
+menuButton.addEventListener("click", () => {
+  clearContainer();
+  initMenu();
+});
+
+function clearContainer() {
+  const container = document.querySelector("#content");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,17 @@
+import explodedHotdog from "./images/exploded-hotdog.jpg";
+import hotdogCheese from "./images/hotdog-cheese.jpg";
 const container = document.querySelector("#content");
 const title = document.createElement("h1");
 
 const menuItems = {
   itemOne: {
     name: "Classic",
-    image: "./images/exploded-hotdog.jpg",
+    image: explodedHotdog,
     description: "The classic microwaved hotdog, perfect for all occasions!",
   },
   itemTwo: {
     name: "Grilled Cheese Dog",
-    image: "./images/hotdog-cheese.jpg",
+    image: hotdogCheese,
     description:
       'A delicious "grilled"(microwaved) hotdog wrapped in american cheese and white bread.',
   },
@@ -23,5 +25,28 @@ function createTitle() {
 
 function createMenu() {
   for (const item in menuItems) {
+    const itemTitle = document.createElement("h2");
+    itemTitle.textContent = menuItems[item].name;
+    itemTitle.className = `${item}-title`;
+
+    const itemDescription = document.createElement("p");
+    itemDescription.textContent = menuItems[item].description;
+    itemDescription.className = `${item}-description`;
+
+    const itemImage = new Image();
+    itemImage.src = menuItems[item].image;
+    itemImage.className = `${item}-image`;
+
+    const menuItem = document.createElement("div");
+    menuItem.append(itemTitle, itemDescription, itemImage);
+    container.appendChild(menuItem);
   }
 }
+
+function initMenu() {
+  createTitle();
+  createMenu();
+  container.className = "menu-content";
+}
+
+export { initMenu };
