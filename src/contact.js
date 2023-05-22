d959813bbad12c4a50f9aa4a6f88d99966fd5fe2
Bjwills7 <bjwills7@hotmail.com>
2023-05-22 17:34:35 -0500

    Add function to create title Add function to create page content from data taken from the contacts object

--- src/contact.js
+++ src/contact.js
@@ -20,10 +20,43 @@ Object.defineProperty(contacts, "length", {
   },
 });
 
-function contactCreate(name, job, phone, email) {
+function addContact(name, job, phone, email) {
   const obj = { name, job, phone, email };
   const objKey = `contact${contacts.length() + 1}`;
   Object.assign(contacts, { [objKey]: { ...obj } });
 }
 
-export { contactCreate, contacts };
+function createTitle() {
+  const title = document.createElement("h1");
+  title.textContent = "Contacts";
+  title.className = "contacts-title";
+  container.appendChild(title);
+}
+
+function createContent() {
+  for (const contact in contacts) {
+    const contactContainer = document.createElement("div");
+    const name = document.createElement("h2");
+    name.textContent = `${contacts[contact].name}`;
+
+    const list = document.createElement("ul");
+    for (const key in contacts[contact]) {
+      if (key.toString() !== "name") {
+        const listItem = document.createElement("li");
+        listItem.textContent = contacts[contact][key];
+        list.appendChild(listItem);
+      }
+    }
+
+    contactContainer.append(name, list);
+    container.appendChild(contactContainer);
+  }
+}
+
+function initContact() {
+  createTitle();
+  createContent();
+  container.className = "contact-content";
+}
+
+export { initContact };
